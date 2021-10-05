a7de9dfb94538d62f587e823f9fe63a5c4e729b2
swang202 <70294941+swang202@users.noreply.github.com>
2021-10-05 02:25:39 -0700

    Challenge part 1

---
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,33 @@
+--Retrieve retirement titles
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER By emp_no;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON(emp_no)
+    emp_no,
+	first_name,
+    last_name,
+    title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- retrieve the number of employees by their most recent job title
+SELECT COUNT(emp_no)
+	emp_no,
+	title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
