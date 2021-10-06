61c2bfaad8d1ae2cbb7b29e6efd5a6a0bb7422c0
swang202 <70294941+swang202@users.noreply.github.com>
2021-10-05 17:15:35 -0700

    Challenge part 2

--- Analysis_Projects_Folder/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Analysis_Projects_Folder/Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -31,3 +31,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(emp_no) DESC;
+
+
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+-- INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_employees AS de
+ON (e.emp_no=de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no=t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND ( e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no 
